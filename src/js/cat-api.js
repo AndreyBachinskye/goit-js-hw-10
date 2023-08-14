0935f7c610c5ec4adcecb224a99aa9094929485b
AndreyBachinskye <andrey099357@gmail.com>
2023-08-14 11:25:00 +0300

    123

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,52 +1,25 @@
-export function fetchBreeds() {
-    const url = 'https://api.thecatapi.com/v1/breeds';
-    const api_key =
-      'live_gjsDAbiVVqU3VKXGaxOhzhjZ0WgBx8tX9HEnAR2MFuFx3Et1Ju66JBqEQRRajf2r';
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-      'x-api-key': api_key,
+import axios from 'axios';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
+  return axios
+    .get(`${BASE_URL}/breeds`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error:', error.message);
     });
-    return fetch(url, { headers })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        return data.map(({ id, name }) => ({ id, name }));
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  
-  export function fetchCatByBreed(breedId) {
-    const api_key =
-      'live_gjsDAbiVVqU3VKXGaxOhzhjZ0WgBx8tX9HEnAR2MFuFx3Et1Ju66JBqEQRRajf2r';
-    const headers = new Headers({
-      'x-api-key': api_key,
+}
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Error:', error.message);
     });
-  
-    const searchParams = new URLSearchParams({
-      breed_ids: breedId,
-    });
-    const url = `https://api.thecatapi.com/v1/images/search?${searchParams}`;
-    return fetch(url, { headers })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => data[0])
-      .then(data => {
-        const { url, height, width, breeds } = data;
-        const { name, temperament, description } = breeds[0];
-  
-        return { url, height, width, name, temperament, description };
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+}
+export { fetchBreeds, fetchCatByBreed };
