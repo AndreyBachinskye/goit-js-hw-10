5da1529d6406ee188ac5c111e2634b91895d4836
AndreyBachinskye <andrey099357@gmail.com>
2023-08-14 11:39:55 +0300

    1

--- src/js/cat-refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  loader: document.querySelector('p.loader'),
-  error: document.querySelector('p.error'),
-  breedSelect: document.querySelector('select.breed-select'),
-  catInfo: document.querySelector('div.cat-info'),
-};
